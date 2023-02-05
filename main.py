5ca08441840967bac2844087711e22e29c9abd61
Hacking <western.ant2@gmail.com>
2023-02-05 03:07:27 +0000

    Main test setup

--- main.py
+++ main.py
@@ -2,27 +2,30 @@ from enums import Fields, TimeMetrics
 from process_data import Reader, Data
 from model_training import Model
 from time_tracking import calculate_time
+from serialize_data import Serializer
 
 
 @calculate_time
 def main():
     data_json = Reader('data.json').read_json()
-    # print(data_json)
+    data = Data(data_json)
 
-    # Iterate over the fields and create the model for each of them.
-    # TODO make it dynamically and separate function OR for each field create a separate model.
+    print(data.original_data.head())
+    print("-----------------")
 
-    data = Data(data_json).create_ds_column()  # Create the ds column (our timeseries column).
-    for field in Fields:
-        print("Creating predictions for the field: {}".format(field))
-        # Skip the x field as it is our DS field.
-        if field == Fields.x:
-            continue
-        data = data.create_y_column(field)
-        print(data.processed_data.head())
-        model = Model(data.processed_data).fit()
-        forecast = model.predict(TimeMetrics.ONE_WEEK)
-        model.plot(forecast)
+    print(data.visits_with_ds.head())
+    print(data.uniques_with_ds.head())
+    print(data.sdur_with_ds.head())
+
+    model = Model(data.visits_with_ds)
+    model.train()
+
+    model = Model(data.uniques_with_ds)
+    model.train()
+
+    model = Model(data.sdur_with_ds)
+    model.train()
+
 
 
 if __name__ == '__main__':
